3f0d1dc8cc83093e375928d7cd48698746fb8c0a
Ben Parisot <me@benparisot.com>
2017-10-28 11:37:53 -0700

    add scripts to slide survey up after submit, fade in result, add margin to center result on page

--- js/scripts.js
+++ js/scripts.js
@@ -20,49 +20,53 @@ $(document).ready(function() {
 
 
     if (q1 === "1") {
-      house1 += 1;
+      date1 += 1;
     } else if (q1 === "2") {
-      house2 += 1;
+      date2 += 1;
     } else if (q1 === "3"){
-      house3 += 1;
+      date3 += 1;
     } else if (q1 === "4") {
-      house4 += 1;
+      date4 += 1;
     }
 
     if (q2 === "1") {
-      house1 += 1;
+      date1 += 1;
     } else if (q2 === "2") {
-      house2 += 1;
+      date2 += 1;
     } else if (q2 === "3"){
-      house3 += 1;
+      date3 += 1;
     } else if (q2 === "4") {
-      house4 += 1;
+      date4 += 1;
     }
 
     if (q3 === "1") {
-      house1 += 1;
+      date1 += 1;
     } else if (q3 === "2") {
-      house2 += 1;
+      date2 += 1;
     } else if (q3 === "3"){
-      house3 += 1;
+      date3 += 1;
     } else if (q3 === "4") {
-      house4 += 1;
-    }
-
-    //console.log("House2:" + house2);
-
-
-    if (house1 > house2 && house1 > house3 && house1 > house4) {
-      $("#stark").show();
-    } else if (house2 > house1 && house2 > house3 && house2 > house4) {
-      $("#lannister").show();
-    } else if (house3 > house1 && house3 > house2 && house3 > house4) {
-      $("#targaryen").show();
-    } else if (house4 > house1 && house4 > house3 && house4 > house2) {
-      $("#greyjoy").show();
+      date4 += 1;
     } else {
-      $("#none").show();
+			alert("Please enter different answers");
+		}
+
+    //console.log("date2:" + date2);
+
+
+    if (date1 > date2 && date1 > date3 && date1 > date4) {
+      $("#meloni").fadeTo(2500, 1);
+    } else if (date2 > date1 && date2 > date3 && date2 > date4) {
+      $("#wes").fadeTo(2500, 1);
+    } else if (date3 > date1 && date3 > date2 && date3 > date4) {
+      $("#kanye").fadeTo(2500, 1);
+    } else if (date4 > date1 && date4 > date3 && date4 > date2) {
+      $("#glover").fadeTo(2500, 1);
     }
+
+		$("#upper").slideUp(1200, function() {
+
+		});
 
     event.preventDefault();
   });
